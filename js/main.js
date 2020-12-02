b68e9092c70146f3831d8b3e35932f9155b97f20
josh choi <wonchoi1994@gmail.com>
2020-12-01 21:37:11 -0800

    event listener added/image preview

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 var userImgInput = document.querySelector("input[name='avatarUrl']");
 var userImg = document.querySelector("#avatarImg");
-// var avatarUrl = document.querySelector('avatarUrl');
 
-userImgInput.addEventListener('input', function (event) {
-//   userImg.src = event.target.value;
-    console.log('event was fired');
-  userImg.setAttribute('src', event.target.value);
+userImgInput.addEventListener("input", function (event) {
+  userImg.setAttribute("src", event.target.value);
 });
