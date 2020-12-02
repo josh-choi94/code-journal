6c83cbbcd7edcdc3a6108b75b32ca019ccc4d415
josh choi <wonchoi1994@gmail.com>
2020-12-01 21:36:18 -0800

    avatar src preview image/input event added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-var userAvatar = document.getElementById('#avatar')
+var userImgInput = document.querySelector("input[name='avatarUrl']");
+var userImg = document.querySelector("#avatarImg");
+// var avatarUrl = document.querySelector('avatarUrl');
 
-userAvatar.addEventListener('input', function(event){
-    
-})
+userImgInput.addEventListener('input', function (event) {
+//   userImg.src = event.target.value;
+    console.log('event was fired');
+  userImg.setAttribute('src', event.target.value);
+});
