58c648e2242d0f5513529767617cfc89140437ed
josh choi <wonchoi1994@gmail.com>
2020-12-02 11:48:32 -0800

    assigned form value into profile data model

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 var userImgInput = document.querySelector("input[name='avatarUrl']");
 var userImg = document.querySelector("#avatarImg");
-var formSelect = document.querySelector('form');
+var userForm = document.querySelector('#contact-form');
 
 userImgInput.addEventListener("input", function (event) {
   userImg.setAttribute("src", event.target.value);
 });
 
 
-formSelect.addEventListener('submit' function(event){
-  
-}
+userForm.addEventListener('submit', function (event){
+  data.profile.avatarUrl = userForm.elements.avatarUrl.value;
+  data.profile.username = userForm.elements.username.value;
+  data.profile.fullName = userForm.elements.fullName.value;
+  data.profile.location = userForm.elements.location.value;
+  data.profile.bio = userForm.elements.bio.value;
+ 
+})
+
+console.log(data.profile);
