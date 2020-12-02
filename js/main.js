7cf44ec97ca35020cc12c37eede255df0556a9b5
josh choi <wonchoi1994@gmail.com>
2020-12-02 11:09:00 -0800

    added listener on form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var userImgInput = document.querySelector("input[name='avatarUrl']");
 var userImg = document.querySelector("#avatarImg");
+var formSelect = document.querySelector('form');
 
 userImgInput.addEventListener("input", function (event) {
   userImg.setAttribute("src", event.target.value);
 });
+
+
+formSelect.addEventListener('submit' function(event){
+  
+}
